e84b41eaecf63f783c7858f0ed15406c9f7e6883
Sanjay707077 <tsanjay7070@gmail.com>
2026-04-15 10:58:16 +0530

    shakeel added UC10

---
+++ src/UC9.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Bogie {
+    String type;
+    int capacity;
+
+    Bogie(String type, int capacity) {
+        this.type = type;
+        this.capacity = capacity;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    @Override
+    public String toString() {
+        return type + " (Capacity: " + capacity + ")";
+    }
+}
+
